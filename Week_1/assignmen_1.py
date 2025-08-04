0070ee811c34a6cbd1cdedab6f437a5c1fc82610
Lorraine254 <lorraine.atsulu@gmail.com>
2025-08-04 15:51:01 +0300

    assignment 1 done

---
+++ Week_1/assignmen_1.py
@@ -0,0 +1,6 @@
+# Taking user input
+x=float(input("Please enter the first number:"))
+y=float(input("Please enter the second number:"))
+
+# Performing a mathematical operation of the numbers
+print(f"The sum of {x} and {y} is {x+y}")
